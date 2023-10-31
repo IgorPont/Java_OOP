260cb6c3dd93ae02cfedfd20eaa7ef2170ca862b
igor_pont <pontigor11@gmail.com>
2023-10-31 21:22:33 +0300

    Added MVP application example

---
+++ Software_architecture/mvp_application/src/main/java/ru/geekbrains/lesson8/Sample01.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.lesson8;
+
+import ru.geekbrains.lesson8.models.TableModel;
+import ru.geekbrains.lesson8.presenters.BookingPresenter;
+import ru.geekbrains.lesson8.views.BookingView;
+
+import java.util.Date;
+
+public class Sample01 {
+
+    /**
+     * ДОМАШНЯЯ РАБОТА
+     * Метод changeReservationTable должен ЗАРАБОТАТЬ!
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        TableModel model = new TableModel();
+        BookingView view = new BookingView();
+        BookingPresenter bookingPresenter = new BookingPresenter(model, view);
+
+        bookingPresenter.loadTables();
+        bookingPresenter.updateView();
+
+        view.reservationTable(new Date(), 3, "Станислав");
+
+    }
+
+}
